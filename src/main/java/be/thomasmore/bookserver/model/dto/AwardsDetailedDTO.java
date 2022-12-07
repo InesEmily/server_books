bd81e8a20ee6999deee68b72b07228f169f51a8f
ines <r0306129@student.thomasmore.be>
2022-12-07 15:11:33 +0100

    AwardsDetailedDTO aangemaakt

--- src/main/java/be/thomasmore/bookserver/model/dto/AwardsDetailedDTO.java
+++ src/main/java/be/thomasmore/bookserver/model/dto/AwardsDetailedDTO.java
@@ -1,4 +1,17 @@
 package be.thomasmore.bookserver.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class AwardsDetailedDTO {
+    private int id;
+    private String name;
+    private String info;
+    private String country;
 }
